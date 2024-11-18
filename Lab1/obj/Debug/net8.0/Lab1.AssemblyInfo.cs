77c78156a4a5ed8a45a7f6b7244b5b70444bd933
Ashley Björs <abs21004@student.mdu.com>
2024-11-18 10:59:11 +0100

    Renamed Project

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4fc94c0d179ec7de0b82c9cac2292cb6bc7c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4091a326dfdc6ee026d1b23ce8b1e517490b7d31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
