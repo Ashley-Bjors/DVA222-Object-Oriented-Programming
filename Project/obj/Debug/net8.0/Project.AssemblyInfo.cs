b5a61e66401749ffc9be1c237e4ce0d3444d499d
Ashley Björs <abs21004@student.mdu.com>
2025-01-03 14:22:03 +0100

    Empty Box working

--- Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fccb8f422e2d6af96f3cd9509b765c1c9969d688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b62de6be0cbafd4cfabf887a4ea1d1cce89661")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
